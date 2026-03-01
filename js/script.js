360ffa6f40d66186bce4e68512f45dd10ef7dd63
unknown <jy298482@dal.ca>
2026-03-01 15:15:12 -0400

    slowed down fish mvmt and made some minor refactors

--- js/script.js
+++ js/script.js
@@ -1,31 +1,53 @@
 const canvas = document.getElementById("gameCanvas");
 const context = canvas.getContext("2d");
 
-// starting coordinates
-let x = 100;
-let y = 200; 
-let speed = 2;
+// canvas dimensions
+const canvasWidth = canvas.width;
+const canvasHeight = canvas.height;
+
+// fish dimensions relative to canvas dimensions
+const fishWidth = canvasWidth * 0.1;
+const fishHeight = canvasHeight * 0.05;
+
+// starting vars
+let x = canvasWidth * 0.1;
+let baseY = canvasHeight * 0.3;
+let y = baseY; 
+
+let speed = 0.2;
 let direction = 1; // 1 is right, -1 is left
-let width = 50;
+let time = 0;
+let swayAmplitude = canvasHeight * 0.03;
+let swayFrequency = 0.005;
+
 
 function update() {
-    // update fish pos, hunger, hp
-    // check collisions, breeding timers
-    x += (speed * direction);
+    let verticalMvmt = baseY + Math.sin(time) * swayAmplitude;
+    let horizontalMvmt = speed * direction;
 
-    if (x + width > canvas.width) {
+    //horizontal mvmt
+    x += horizontalMvmt;
+
+    // vertical mvmt
+    time += swayFrequency;
+    y = verticalMvmt;
+
+    // horizontal boundaries
+    if (x + fishWidth > canvasWidth) {
+        x = canvasWidth - fishWidth;
         direction = -1;
     }
 
-    if (x < 0) {
+    if (x <= 0) {
+        x = 0;
         direction = 1;
     }
 }
 
 function draw() {
-    context.clearRect(0, 0, canvas.width, canvas.height);
+    context.clearRect(0, 0, canvasWidth, canvasHeight);
     context.fillStyle = "pink";
-    context.fillRect(x, y, 50, 30);
+    context.fillRect(x, y, fishWidth, fishHeight);
 }
 
 // game loop continuously plays the 'scene' displayed on screen so that there is animation
